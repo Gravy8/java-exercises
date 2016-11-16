dcc52fd538db702b56ce4ae9a0f74616698cd50a
Brad Colton <bradcolton11@yahoo.com>
2016-11-16 10:31:09 -0600

    added guess counter and created a max number of guesses before game is lost

--- src/GameDevelopment101.java
+++ src/GameDevelopment101.java
@@ -4,10 +4,11 @@ public class GameDevelopment101 {
     public static void main(String[] args) {
         int randomNumber;
         int userEntry = 0;
+        int guesses = 0;
         Scanner sc = new Scanner(System.in);
         randomNumber = (int) Math.floor(Math.random() * 100) + 1;
         while(userEntry != randomNumber){
-            System.out.print("Guess a number from 1 - 100: ");
+            System.out.print((5-guesses) + " Guesses remaining\nGuess a number from 1 - 100: ");
             try{
                 userEntry = sc.nextInt();
                 if (userEntry <= 0 || userEntry > 100){
@@ -20,11 +21,20 @@ public class GameDevelopment101 {
                 continue;
             }
             if (userEntry > randomNumber){
-                System.out.println("The number is lower");
+                guesses++;
+                System.out.println("The number is lower\n");
             } else if(userEntry < randomNumber){
+                guesses++;
                 System.out.println("The number is higher");
             }
+            if (guesses == 5){
+                break;
+            }
         }
-        System.out.println("Good Guess!");
+        if(guesses == 5){
+            System.out.println("The number was " + randomNumber + " Game Over");
+        }else{
+            System.out.println("Good Guess!");
+        }
     }
 }

a615c19038ffe05ef3b7ec3d7f21a1e2ad0e33de
Brad Colton <bradcolton11@yahoo.com>
2016-11-28 14:32:58 -0600

    created student object constructor class

---
+++ src/Student.java
@@ -0,0 +1,11 @@
+public class Student {
+    String firstName;
+    String lastName;
+    int score;
+
+    Student(String firstName, String lastName, int score){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.score = score;
+    }
+}

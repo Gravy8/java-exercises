2c236060a087eb8dcbc273e910180eb02a88c431
Brad Colton <bradcolton11@yahoo.com>
2016-11-28 15:36:32 -0600

    created resuable keep going checker to return a boolean for whether or not the user wants to coninue entering data

---
+++ src/KeepGoing.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+import java.util.InputMismatchException;
+public class KeepGoing {
+    public static boolean check(String prompt){
+        Scanner sc = new Scanner(System.in);
+        String keepGoing;
+        while(true) {
+            try {
+                System.out.print(prompt);
+                keepGoing = sc.next();
+                if (!keepGoing.equalsIgnoreCase("y") && !keepGoing.equalsIgnoreCase("n")) {
+                    System.out.println("Please enter a valid character");
+                    continue;
+                }
+                if(keepGoing.equalsIgnoreCase("y")){
+                    return true;
+                }
+                return false;
+            } catch (InputMismatchException e) {
+                sc.next();
+                System.out.println("Please enter a valid character");
+            }
+        }
+    }
+}
